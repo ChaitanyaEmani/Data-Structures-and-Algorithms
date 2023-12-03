13a3aab2bd886202faf8bf2e30b9dd5b08b85fc9
ChaitanyaEmani <131893680+ChaitanyaEmani@users.noreply.github.com>
2023-12-03 08:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (47.86%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> mat (n, vector<int>(n));
-        int num = 1;
-        int r1 = 0, r2 = n - 1, c1 = n - 1, c2 = 0;
-        while (num <= n * n) {
-            for (int i = r1; i <= r2; i++) {
-                mat[r1][i] = num;
-                num++;
+        vector<vector<int>> mat(n, vector<int>(n,1));
+        int index = 1;
+        int left = 0, right= n-1, top = 0, down = n-1;
+        while(left <= right && top <= down){
+            for(int j = left; j <= right; j++){
+               mat[top][j] = index;
+                index++;
             }
-            r1++;
-            for (int i = c2 + 1; i <= c1; i++) {
-                mat[i][c1] = num;
-                num++;
+            top++;
+            for(int i = top; i <= down; i++){
+                mat[i][right] = index;
+                index++;
             }
-            c1--;
-            for (int i = r2 - 1; i >= r1 - 1; i--) {
-                mat[r2][i] = num;
-                num++;
+           
+            right--;
+            for(int j = right; j >= left; j--){
+                mat[down][j] = index;
+                index++;
             }
-            r2--;
-            for (int i = c1; i >= c2 + 1; i--) {
-                mat[i][c2] = num;
-                num++;
-            }
-            c2++;
+            down--;
+            for(int i = down; i>=top; i--){
+                    mat[i][left] = index;
+                    index++;
+                }
+            left++;
         }
         return mat;
+
     }
 };
